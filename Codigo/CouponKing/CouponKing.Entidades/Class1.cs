1a796b1ec0b9ffd246f2bd0d168082a098bf048b
Matapluche <matapluche@gmail.com>
2013-09-28 12:26:45 -0500

    Montaje arquitectura

---
+++ Codigo/CouponKing/CouponKing.Entidades/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CouponKing.Entidades
+{
+    public class Class1
+    {
+    }
+}
---
+++ Codigo/CouponKing/CouponKing.Logica/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CouponKing.Logica
+{
+    public class Class1
+    {
+    }
+}
